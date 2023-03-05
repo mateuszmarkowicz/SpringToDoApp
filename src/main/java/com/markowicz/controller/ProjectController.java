5ed906fdcc3ab092c7eb56ab030a4255ec517280
Mateusz Markowicz <markowiczm99@wp.pl>
2023-03-05 17:50:51 +0100

    before zad 12

--- src/main/java/com/markowicz/controller/ProjectController.java
+++ src/main/java/com/markowicz/controller/ProjectController.java
@@ -1,18 +1,76 @@
 package com.markowicz.controller;
 
+import com.markowicz.logic.ProjectService;
+import com.markowicz.model.Project;
+import com.markowicz.model.ProjectStep;
 import com.markowicz.model.projection.ProjectWriteModel;
+import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 @RequestMapping("/projects")
 class ProjectController {
+    private final ProjectService service;
+
+    ProjectController(ProjectService service) {
+        this.service = service;
+    }
 
     @GetMapping
     String showProjects(Model model){
         model.addAttribute("project", new ProjectWriteModel());
         return "projects";
     }
+
+    @PostMapping
+    String addProject(
+            @ModelAttribute("project") @Valid ProjectWriteModel current,
+                      BindingResult bindingResult,
+                      Model model
+    ){
+        if(bindingResult.hasErrors()){
+            return "projects";
+        }
+        service.createProject(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("projects", getProjects());
+        model.addAttribute("message", "Dodano projekt!");
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current){
+        current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    ){
+        try {
+            service.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę!");
+        }catch (IllegalStateException | IllegalArgumentException e){
+            model.addAttribute("message", "Błąd podczas tworzenia grupy!");
+        }
+        return "projects";
+    }
+
+    @ModelAttribute("projects")
+    List<Project> getProjects(){
+        return service.readAll();
+    }
+
+
 }
--- src/test/java/com/markowicz/TestConfiguration.java
+++ src/test/java/com/markowicz/TestConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Configuration
@@ -55,6 +56,11 @@ class TestConfiguration {
            @Override
            public List<Task> findAllByGroup_Id(Integer id) {
                return List.of();
+           }
+
+           @Override
+           public List<Task> findAllByDeadlineNullOrDeadlineBefore(LocalDateTime today) {
+               return null;
            }
 
            @Override
